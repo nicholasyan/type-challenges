f5193bc2a9f8f4fc3cd283e41d1ad42157f4321a
Nicholas Yan <nicholasyan11@gmail.com>
2022-04-14 23:48:17 -0700

    18-easy-tuple-length

---
+++ questions_solved/14-easy-first/answer.ts
@@ -0,0 +1,17 @@
+import { Equal, Expect } from '@type-challenges/utils'
+
+type First<T extends any[]> = T extends [infer P, ...any] ? P : never;
+
+type cases = [
+  Expect<Equal<First<[3, 2, 1]>, 3>>,
+  Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
+  Expect<Equal<First<[]>, never>>,
+  Expect<Equal<First<[undefined]>, undefined>>
+]
+
+type errors = [
+  // @ts-expect-error
+  First<'notArray'>,
+  // @ts-expect-error
+  First<{ 0: 'arrayLike' }>
+]
