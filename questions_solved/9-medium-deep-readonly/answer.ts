bd61617832e3778a502433fc1bc38a018a1aac30
Nicholas Yan <nicholasyan11@gmail.com>
2022-04-15 12:53:16 -0700

    9-medium-deep-readonly

---
+++ questions_solved/9-medium-deep-readonly/answer.ts
@@ -0,0 +1,60 @@
+import { Equal, Expect } from '@type-challenges/utils'
+
+type DeepReadonly<T> = 
+  T extends Function
+    ? T
+    : T extends object 
+      ? {
+        readonly [k in keyof T]: DeepReadonly<T[k]>;
+      } 
+      : T;
+
+type cases = [
+  Expect<Equal<DeepReadonly<X>, Expected>>,
+]
+
+type X = {
+  a: () => 22
+  b: string
+  c: {
+    d: boolean
+    e: {
+      g: {
+        h: {
+          i: true
+          j: 'string'
+        }
+        k: 'hello'
+      }
+      l: [
+        'hi',
+        {
+          m: ['hey']
+        }
+      ]
+    }
+  }
+}
+
+type Expected = {
+  readonly a: () => 22
+  readonly b: string
+  readonly c: {
+    readonly d: boolean
+    readonly e: {
+      readonly g: {
+        readonly h: {
+          readonly i: true
+          readonly j: 'string'
+        }
+        readonly k: 'hello'
+      }
+      readonly l: readonly [
+        'hi',
+        {
+          readonly m: readonly ['hey']
+        }
+      ]
+    }
+  }
+}
