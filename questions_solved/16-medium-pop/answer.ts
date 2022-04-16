0fe997580b0a192830b74a41224c5ea1f0621728
Nicholas Yan <nicholasyan11@gmail.com>
2022-04-16 10:50:38 -0700

    16-medium-pop

---
+++ questions_solved/16-medium-pop/answer.ts
@@ -0,0 +1,9 @@
+import { Equal, Expect } from '@type-challenges/utils'
+
+type Pop<T extends any[]> = T extends [...infer First, any] ? First : [];
+
+type cases = [
+  Expect<Equal<Pop<[3, 2, 1]>, [3, 2]>>,
+  Expect<Equal<Pop<['a', 'b', 'c', 'd', ]>, ['a', 'b', 'c']>>,
+  Expect<Equal<Pop<[]>, []>>,
+]
