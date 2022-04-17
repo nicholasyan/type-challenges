df3a20e6ad7dbeeffb1a0409bcacd91990115a4e
Nicholas Yan <nicholasyan11@gmail.com>
2022-04-17 16:14:15 -0700

    119-medium-replaceall

---
+++ questions_solved/119-medium-replaceall/answer.ts
@@ -0,0 +1,20 @@
+import { Equal, Expect } from '@type-challenges/utils'
+
+type ReplaceAll<S extends string, From extends string, To extends string> = 
+  From extends ''
+    ? S
+    : S extends `${infer Start}${From}${infer End}`
+      ? `${Start}${To}${ReplaceAll<End, From, To>}`
+      : S;
+
+type cases = [
+  Expect<Equal<ReplaceAll<'foobar', 'bar', 'foo'>, 'foofoo'>>,
+  Expect<Equal<ReplaceAll<'foobar', 'bag', 'foo'>, 'foobar'>>,
+  Expect<Equal<ReplaceAll<'foobarbar', 'bar', 'foo'>, 'foofoofoo'>>,
+  Expect<Equal<ReplaceAll<'t y p e s', ' ', ''>, 'types'>>,
+  Expect<Equal<ReplaceAll<'foobarbar', '', 'foo'>, 'foobarbar'>>,
+  Expect<Equal<ReplaceAll<'barfoo', 'bar', 'foo'>, 'foofoo'>>,
+  Expect<Equal<ReplaceAll<'foobarfoobar', 'ob', 'b'>, 'fobarfobar'>>,
+  Expect<Equal<ReplaceAll<'foboorfoboar', 'bo', 'b'>, 'foborfobar'>>,
+  Expect<Equal<ReplaceAll<'', '', ''>, ''>>,
+]
